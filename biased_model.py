3d776648813f40c33d872a2a63aa3a33b5fa6141
Riddhi Patel <riddhiha@usc.edu>
2022-02-26 19:13:40 -0800

    commit biased_model

---
+++ biased_model.py
@@ -0,0 +1,61 @@
+import json
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+data = pd.read_csv('./data/adult.csv')
+data.head()
+
+#Data processing - missing values
+data['occupation'].replace(to_replace ="?", 
+                 value = "Miscellaneous", 
+                  inplace = True)
+
+# Label Encoding
+le1=LabelEncoder()
+occupation=le1.fit_transform(data['occupation'])
+occ = dict(zip(occupation, data.occupation))
+
+le2 = LabelEncoder()
+gender = le2.fit_transform(data['gender'])
+gen = dict(zip(gender, data.gender))
+
+# print(occ)
+# print(gen)
+
+#updating the data with new labels
+data.occupation = occupation
+data.gender = gender
+
+# defining parameters on which model will be trained
+X = data[['age','occupation']]
+#desired output
+Y = data['income']
+
+#split data into training and testing model
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1, random_state = 23)
+
+#Training the model 
+bias = OneVsRestClassifier(RandomForestClassifier(),n_jobs=-1)
+bias.fit(X_train,Y_train)
+
+#accuracy
+print(bias.score(X_train,Y_train))
+
+
+
+
+
+
+
+
+
+
+
